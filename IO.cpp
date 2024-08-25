02f080c44497a2652ca84ea37a7b200a03187809
rolgordijn <derousrian@gmai.com>
2024-08-25 12:08:57 +0200

    Refactor IO library to reduce code duplication

--- IO.cpp
+++ IO.cpp
@@ -2,25 +2,89 @@
 
 IO::IO() {
   changed = true;
-  pin = 0 ;
+  pin = 0;
   tris = 1;
   pinState = 0;
   changed = 0;
 }
 
-void IO::setHigh() {
-  setValue(1);
+
+void IO::init(int dir, int level) {
+  setValue(level);
+  setPinMode(dir);
 }
 
-void IO:: setLow() {
-  setValue(0);
+void IO::setValue(bool val) {
+  val ? this->setHigh() : this->setLow();
+}
+
+void IO::setPinMode(int dir) {
+  (dir == INPUT) ? this->setInput() : this->setOutput();
+}
+
+[[deprecated("Use turnOn() instead")]] void IO::setHigh(void) {
+  if ((pinState ^ invertedLogic) == HIGH) return;
+  setPinState(HIGH ^ invertedLogic);
+  pinState = HIGH ^ invertedLogic;
+  changed = true;
+}
+
+void IO::turnOn(void) {
+  if ((pinState ^ invertedLogic) == HIGH) return;
+  setPinState(HIGH ^ invertedLogic);
+  pinState = HIGH ^ invertedLogic;
+  changed = true;
+}
+
+
+[[deprecated("Use turnOff() instead")]] void IO::setLow(void) {
+  if ((pinState ^ invertedLogic) == LOW) return;
+  setPinState(pinState ^ invertedLogic);
+  pinState = LOW ^ invertedLogic;
+  changed = true;
+}
+
+void IO::turnOff(void) {
+  if ((pinState ^ invertedLogic) == LOW) return;
+  setPinState(pinState ^ invertedLogic);
+  pinState = LOW ^ invertedLogic;
+  changed = true;
 }
 
 void IO::clearChangedFlag() {
-  this->changed = false;
+  changed = false;
 }
 
 bool IO::didChange(void) {
-  return this->changed;
+  return changed;
 }
 
+void IO::setInvertedLogic(bool invert) {
+  invertedLogic = invert;
+}
+
+bool IO::isInvertedLogic() {
+  return invertedLogic;
+}
+
+int IO::getValue(void) {
+  bool currentPinState = readPinState() ^ invertedLogic;
+  if (currentPinState != pinState) {
+    this->changed = true;
+    this->pinState = currentPinState;
+  }
+  return pinState;
+}
+
+void IO::setNameAndIndex(String pinName, int pinIndex) {
+  name = pinName;
+  index = pinIndex;
+}
+
+void IO::debug() {
+  //const char* state = (pinState == (invertedLogic ? LED_ON : LED_OFF)) ? "ON" : "OFF";
+  Serial.print(name);
+  Serial.print(index);
+  Serial.print(f("is ");
+  Serial.println("lol");
+}
--- IO.h
+++ IO.h
@@ -2,63 +2,82 @@
 
 #include "MCP23017.h"
 
+#include <String.h> 
+
 
 class IO {
-  protected:
-    uint8_t pin;
-    bool tris;
-    bool pinState;
-    bool changed;
 
-   
-    
-  public:
-    IO();
-    void setHigh();
-    void setLow();
-    virtual void init(int dir, int level) = 0;
-    virtual bool getValue() = 0;
-    virtual void setValue(bool val) = 0;
-    virtual void setInput()  = 0;
-    virtual void setOutput()  = 0;
-    virtual void setPinMode(int dir) =   0;
-    virtual int getPinMode (void) = 0;
-    void clearChangedFlag();
-    bool didChange(void);
+protected:
+  uint8_t pin;
+  bool tris;
+  bool pinState;
+  bool changed;
+
+  bool invertedLogic;
+  
+  int index;
+  String name;
+
+
+
+  virtual bool readPinState() const = 0;
+  virtual void setPinState(bool level) = 0;
+
+public:
+
+
+  IO();
+  [[deprecated("Use turnOn() instead")]]
+  void setHigh();
+  [[deprecated("Use turnOff() instead")]]
+  void setLow();
+  void turnOn();
+  void turnOff();
+  int getValue(void);
+  void setValue(bool val);
+  virtual void setInput() = 0;
+  virtual void setOutput() = 0;
+  void setPinMode(int dir);
+  virtual int getPinMode(void) = 0;
+  void clearChangedFlag();
+  bool didChange(void);
+  bool isInvertedLogic();
+  void init(int dir, int level);
+  void setInvertedLogic(bool invert);
+  void setNameAndIndex(String pinName, int pinIndex);
+  void debug();
 };
 
 class BasicIO : public IO {
-  public:
-    BasicIO();
-    BasicIO(int pin, int tris, int val);
-    BasicIO(int pin, int tris);
-    void init(int dir, int level);
-    void setHigh(void);
-    void setLow(void);
-    bool getValue(void);
-    void setValue(bool val);
-    void setInput();
-    void setOutput();
-    void setPinMode(int dir);
-    int getPinMode(void);
+private:
 
+public:
+  BasicIO();
+  BasicIO(int pin, int tris, int val);
+  BasicIO(int pin, int tris);
+
+  
+  void setInput();
+  void setOutput();
+  int getPinMode(void);
+
+  bool readPinState() const override;
+  void setPinState(bool level) override;
 };
 
 class MCP23017IO : public IO {
-  private:
-    MCP23017 * ic;
-    bool pinState;
-  public:
-    MCP23017IO();
-    MCP23017IO(MCP23017 * ic, uint8_t pin);
-    void init(int dir, int level);
-    void setHigh(void);
-    void setLow(void);
-    bool getValue();
-    void setValue(bool val);
-    void setInput();
-    void setOutput();
-    void setPinMode(int dir);
-    int getPinMode(void);
+private:
+  MCP23017* ic;
+  bool pinState;
+  
+public:
+  MCP23017IO();
+  MCP23017IO(MCP23017* ic, uint8_t pin);
 
+  void setPinState(bool level) override;
+  bool readPinState() const override;
+
+  void setInput();
+  void setOutput();
+  int getPinMode(void);
 };
